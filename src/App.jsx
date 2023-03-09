2f9c2f3accf26e7f46ca5f907d7c5e3ea36dc962
Sneyder Silva <sneyderst94@gmail.com>
2023-03-09 18:15:51 -0300

    Dark mode toggle

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,17 @@ import axios from 'axios';
 const App = () => {
   const [data, setData] = useState(null);
   const [bg, setBg] = useState(null);
+  const [s, setS] = useState('3%');
+
+  const toggleBackground = () => {
+    setBg(
+      bg ===
+        'https://www.infocampo.com.ar/wp-content/uploads/2020/07/Clima-Tiempo-despejado-Trigo.jpg'
+        ? 'https://cdn.pixabay.com/photo/2017/10/12/18/29/night-2845347_1280.jpg'
+        : 'https://www.infocampo.com.ar/wp-content/uploads/2020/07/Clima-Tiempo-despejado-Trigo.jpg',
+    );
+    setS(s === '3%' ? '60%' : '3%');
+  };
 
   const getWeather = (lat, lon) => {
     axios
@@ -32,13 +43,16 @@ const App = () => {
 
   return (
     <div
-      className="App flex justify-center items-center flex-col"
+      className="App flex justify-around items-center flex-col"
       style={{
         backgroundImage: `url("${bg}")`,
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'cover',
       }}
     >
+      <button className="toggle" onClick={toggleBackground}>
+        <p className="span" style={{ marginLeft: `${s}`, transition: '0.6s' }}></p>
+      </button>
       <Card data={data} />
       <footer>Developed by: Sneyder Silva.</footer>
     </div>
